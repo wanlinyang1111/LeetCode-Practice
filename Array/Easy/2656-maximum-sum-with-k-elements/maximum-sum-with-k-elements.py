4572213a3cfef0998351927620dde625d6a9d856
wanlinyang <wanlinyang1111@gmail.com>
2024-10-23 21:34:26 +0800

    Update maximum-sum-with-k-elements.py

--- Array/Easy/2656-maximum-sum-with-k-elements/maximum-sum-with-k-elements.py
+++ Array/Easy/2656-maximum-sum-with-k-elements/maximum-sum-with-k-elements.py
@@ -1,1 +1,9 @@
-"" 
+class Solution:
+    def maximizeSum(self, nums: List[int], k: int) -> int:
+        score=0
+
+        for num in range(k):  
+            m=max(nums)
+            score+=m
+            nums.append(m+1)
+        return score
