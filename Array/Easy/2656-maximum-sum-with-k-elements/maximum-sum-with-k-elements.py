c98f07c5b5ffbffdc32aa3585a3d4c6d77db5092
wanlinyang <wanlinyang1111@gmail.com>
2024-10-24 15:39:04 +0800

    Update maximum-sum-with-k-elements.py

--- Array/Easy/2656-maximum-sum-with-k-elements/maximum-sum-with-k-elements.py
+++ Array/Easy/2656-maximum-sum-with-k-elements/maximum-sum-with-k-elements.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximizeSum(self, nums: List[int], k: int) -> int:
-        score=0            #initialize the score
+        score=0    #initialize the score
 
         #loop k times to select k maximum values
         for num in range(k):  
-            m=max(nums)            #get the current maximum
-            score+=m            #add to the total score
-            nums.append(m+1)            #remove the selected maximum
-        return score            #return the total score
+            m=max(nums)    #get the current maximum
+            score+=m    #add to the total score
+            nums.append(m+1)    #remove the selected maximum
+        return score    #return the total score
