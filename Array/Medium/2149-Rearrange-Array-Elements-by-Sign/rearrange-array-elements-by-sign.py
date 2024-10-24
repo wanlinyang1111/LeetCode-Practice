71a2ca1005c8a38d4dd09c9c4230835736a29dbf
wanlinyang <wanlinyang1111@gmail.com>
2024-10-24 15:36:41 +0800

    Update rearrange-array-elements-by-sign.py

--- Array/Medium/2149-Rearrange-Array-Elements-by-Sign/rearrange-array-elements-by-sign.py
+++ Array/Medium/2149-Rearrange-Array-Elements-by-Sign/rearrange-array-elements-by-sign.py
@@ -1,1 +1,18 @@
-"" 
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        pos_arr = []  # Initialize an empty list for positive numbers
+        neg_arr = []  # Initialize an empty list for negative numbers
+
+        for num in nums:  # Iterate through each number in the input list
+            if num > 0:
+                pos_arr.append(num)  # Append positive numbers to pos_arr
+            else:
+                neg_arr.append(num)  # Append negative numbers to neg_arr
+
+        ans_arr = []  # Initialize an empty list for the final rearranged array
+
+        for i in range(len(pos_arr)):  # Loop through the range of the length of pos_arr
+            ans_arr.append(pos_arr[i])  # Append the positive number at index i
+            ans_arr.append(neg_arr[i])  # Append the negative number at index i
+        
+        return ans_arr  # Return the final rearranged array
