b51ce1debb36b02e5a255e89b60d2abd99f2b802
wanlinyang <wanlinyang1111@gmail.com>
2024-10-28 19:48:00 +0800

    Update contains-duplicate.py

--- Hash/Easy/217-Contains-Duplicate/contains-duplicate.py
+++ Hash/Easy/217-Contains-Duplicate/contains-duplicate.py
@@ -1,11 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        dic = {}  # Initialize an empty dictionary to store occurrences of numbers
+        dic = {}              　# Initialize an empty dictionary to store occurrences of numbers
 
-        for num in nums:    # Iterate through each number in the input list
-            if num not in dic:    # Check if the number is not already in the dictionary
+        for num in nums:        # Iterate through each number in the input list
+            if num not in dic:  # Check if the number is not already in the dictionary
                 dic[num] = 1    # Add the number to the dictionary with a count of 1  
-            else:    # If the number is found again, return True indicating a duplicate
+            else:               # If the number is found again, return True indicating a duplicate
                 return True  
-        return False    # If no duplicates were found, return False 
+        return False            # If no duplicates were found, return False 
