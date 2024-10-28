ad0fca2b54b45a70b8f98be9a9b0d247be2c1d7e
wanlinyang <wanlinyang1111@gmail.com>
2024-10-28 19:47:08 +0800

    Update find-all-duplicates-in-an-Array.py

--- Array/Medium/442-Find-All-Duplicates-in-an-Array/find-all-duplicates-in-an-Array.py
+++ Array/Medium/442-Find-All-Duplicates-in-an-Array/find-all-duplicates-in-an-Array.py
@@ -1,1 +1,13 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+
+      ans_arr = []                            # First, I will initialize an empty array to store the answer.
+
+      for num in nums:                        # Next, I will iterate through the `nums` array for each number. 
+          index = abs(num) - 1                # I will calculate the index based on the absolute value of the number.　Using the abs function to get the index.
+          if nums[index] > 0:                 # If the value at this index is greater than zero,
+              nums[index] = -nums[index]      # I will mark it as negative.
+          else:
+              ans_arr.append(abs(num))        # Otherwise, I will append the absolute value of the number to the answer array.
+      return ans_arr                          # Finally, after iterating through the entire array, I will return the answer array.
 
