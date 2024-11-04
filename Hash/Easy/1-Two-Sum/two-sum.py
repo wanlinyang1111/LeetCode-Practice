972b46d4d4f543ddf9ab75b19c2670f8dfce99b9
wanlinyang <wanlinyang1111@gmail.com>
2024-11-04 19:34:13 +0800

    Update two-sum.py

--- Hash/Easy/1-Two-Sum/two-sum.py
+++ Hash/Easy/1-Two-Sum/two-sum.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_to_index = {}        　                   # Dictionary to store numbers and their indices
+        
+        for index, num in enumerate(nums):            # Loop through nums with both index and value
+            remain = target - num                     # Calculate the required complement
+            
+            if remain in num_to_index:                # Check if complement exists in the dictionary
+                return [index, num_to_index[remain]]  # Return current index and complement's index
+            else:
+                num_to_index[num] = index             # Store current number and its index
